b1ad45f3164e8134673a1f7be920c5563eacf9ee
Behrad <behrad.bhrmi@gmail.com>
2022-09-11 13:51:06 +0430

    add aboutus page

--- pages/urls.py
+++ pages/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
 
+
+from .views import HomePageView, AboutUsView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('aboutus/', AboutUsView.as_view(), name='aboutus'),
+
+
+]
